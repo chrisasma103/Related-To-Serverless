1f77d1c0583d48b83ed47fbfdba5f4c68a91b5ba
casma103 <41581802+chrisasma103@users.noreply.github.com>
2021-06-24 13:59:02 -0400

    Finished Step #6

--- Twocatz/index.js
+++ Twocatz/index.js
@@ -1,15 +1,16 @@
 const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    let resp = await fetch("https://251a9d93-8c22-472d-b5c5-c0b013b15125.mock.pstmn.io/bitproject", {
+    let resp = await fetch("https://cataas.com/cat/says/serverless", {
         method: 'GET'
     });
     
     let data = await resp.arrayBuffer()
     // we need to receive it as a buffer since this is an image we are receiving from the API
     // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
+    var base64data = Buffer.from(data).toString('base64')
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: {base64data}
     };
 }
