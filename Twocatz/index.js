e512975648116e2e1a632d2971534b7c082f062f
casma103 <41581802+chrisasma103@users.noreply.github.com>
2021-06-24 14:12:13 -0400

    fixed capital error

--- Twocatz/index.js
+++ Twocatz/index.js
@@ -5,10 +5,10 @@ module.exports = async function (context, req) {
         method: 'GET'
     });
     
-    let data = await resp.arrayBuffer()
+    let firstdata = await resp.arrayBuffer()
     // we need to receive it as a buffer since this is an image we are receiving from the API
     // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
-    var base64data = Buffer.from(data).toString('base64')
+    var base64data = Buffer.from(firstdata).toString('base64')
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: {base64data}
