927c794f5425e0d0a3f9c8f348b5f8aca113f87a
casma103 <41581802+chrisasma103@users.noreply.github.com>
2021-07-15 17:22:34 -0400

    Took notes from the Loom lesson

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,26 +1,32 @@
 async function getImage(event){
     event.preventDefault()
-    var namevalue=document.getElementById("name").value;
-    if (namevalue = ''){
+    var myform = document.getElementById("myform");
+    let nameInput = document.getElementById("username");
+    let fileInput = document.getElementById("image");
+    let file = fileInput.files[0]
+
+    var payload = new FormData(myform);
+    console.log(payload)
+    payload.append("file", file);
+    $('#output').text("Thanks!")
+
+    if (nameInput.value != ''){
+        try {
+            const response = await fetch(process.env.bunnimage-upload-link, {
+                method: "POST" ,
+                body: payload , 
+                headers: {
+                    'codename': nameInput.value
+                }
+            });
+            $('output').text("Your image has been stored successfully!")
+
+        }
+        catch(e){
+            $('output').text(err)
+        }
+    }
+    else{
         alert("No name error.")
     }
-
-    var bunniForm = document.getElementById("myform");
-    var payload = new FormData(bunniForm);
-    const file = fileInput.files[0];
-     payload.append(namevalue, file);
-
-    var myHeaders = new Headers();
-    myHeaders.append("content-type", "file")
-    myHeaders.append("codename",namevalue)
-    try {
-        const response = await fetch(process.env.bunnimage-upload-link, {
-            method: "POST" ,
-            body: payload , 
-            headers: myHeaders
-        });
-    } catch (e){
-        alert("An error occurred.")
-    }
-    $('#output').text("Your image has been stored successfully!")
 }
