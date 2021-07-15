803ee704b0655d6af9dfd238e2b7a5e1dcd39a21
casma103 <41581802+chrisasma103@users.noreply.github.com>
2021-07-14 20:44:35 -0400

    Let's at least try.....

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,9 +1,25 @@
 function getImage(event){
     event.preventDefault()
-    if (document.getElementById("name").value != ''){
-        $('#output').text("Thanks!")
-    }
-    else {
+    var namevalue=document.getElementById("name").value;
+    if (namevalue = ''){
         alert("No name error.")
     }
+
+    var bunniForm = document.getElementById("myform");
+    var payload = new FormData(bunniForm);
+    payload.append(namevalue, document.getElementById("image"));
+
+    var myHeaders = new Headers();
+    myHeaders.append("content-type", "file")
+    myHeaders.append("codename",namevalue)
+    try {
+        const response = await fetch(process.env.bunnimage-upload-link, {
+            method: "POST" ,
+            body: payload , 
+            headers: myHeaders
+        });
+    } catch (e){
+        alert("An error occurred.")
+    }
+    $('#output').text("Uploaded successfully!")
 }
