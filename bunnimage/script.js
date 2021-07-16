f495c545452098aa542e2a860bbfb20750c58ce8
casma103 <41581802+chrisasma103@users.noreply.github.com>
2021-07-16 11:31:53 -0400

    Did step 4???

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -3,26 +3,43 @@ async function getImage(event) {
     var myform = document.getElementById("myform")
     var payload = new FormData(myform);
     console.log(payload)
-    if (document.getElementById("username").value != '') {
+    var username = document.getElementById("username").value;
+    if (username != '') {
         $('#output').text("Thanks!")
   
         console.log("Posting your image...");
         const resp = await fetch("https://bunnimagefunctiongroup.azurewebsites.net/api/bunnimage-upload?code=We5LtNYyyaj8X3gId2EzF/xXK/KjvCOB5L5BTaB4mvdGLc2GPyMbcQ==", {
             method: 'POST',
             headers: {
-                'codename' : document.getElementById("username").value
+                'codename' : username
             },
             body: payload
         });
-        
-        try { 
-            var data = await resp.text();
-            console.log(data);
-            $('#output').text("Your image has been stored successfully!")
-        } catch (e) {
-            alert("Backend error!")
-        }
+  
+        var data = await resp.json();
+        console.log(data);
+        $('#output').text("Your image has been stored!")
     } else {
         alert("No name error.")
     }
+  }
+  
+  async function downloadImage() {
+    var username = document.getElementById("downloadusername").value;
+    console.log("Attempting to get your pdf...");
+    const resp = await fetch("https://bunnimagefunctiongroup.azurewebsites.net/api/bunnimage-download?code=YthJjC6ASn1FQdwfUrFEaPkl/5a1a/bsdDomK9nkkp6Z3dAltO1ciQ==", {
+        method: 'GET',
+        headers: {
+            'username' : username
+        },
+    });
+  
+    var data = await resp.json();
+    console.log("PDF link received!")
+    console.log(data.downloadUri)
+    console.log(data.success)
+    const link = data.downloadUri
+    var success = data.success
+  
+    window.open(link, "_self")
   }
