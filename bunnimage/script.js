326aef29419e61d0ac3f8f5a54d51c84b3d87631
casma103 <41581802+chrisasma103@users.noreply.github.com>
2021-07-16 11:03:36 -0400

    Fixed step3 for realsies

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,32 +1,28 @@
-async function getImage(event){
+async function getImage(event) {
     event.preventDefault()
-    var myform = document.getElementById("myform");
-    let nameInput = document.getElementById("username");
-    let fileInput = document.getElementById("image");
-    let file = fileInput.files[0]
-
+    var myform = document.getElementById("myform")
     var payload = new FormData(myform);
     console.log(payload)
-    payload.append("file", file);
-    $('#output').text("Thanks!")
-
-    if (nameInput.value != ''){
-        try {
-            const response = await fetch(process.env.bunnimage-upload-link, {
-                method: "POST" ,
-                body: payload , 
-                headers: {
-                    'codename': nameInput.value
-                }
-            });
-            $('output').text("Your image has been stored successfully!")
-
+    if (document.getElementById("username").value != '') {
+        $('#output').text("Thanks!")
+  
+        console.log("Posting your image...");
+        const resp = await fetch("https://bunnimagefunctiongroup.azurewebsites.net/api/bunnimage-upload?code=We5LtNYyyaj8X3gId2EzF/xXK/KjvCOB5L5BTaB4mvdGLc2GPyMbcQ==", {
+            method: 'POST',
+            headers: {
+                'codename' : document.getElementById("username").value
+            },
+            body: payload
+        });
+        
+        try { 
+            var data = await resp.text();
+            console.log(data);
+            $('#output').text("Your image has been stored successfully!")
+        } catch (e) {
+            alert("Backend error!")
         }
-        catch(e){
-            $('output').text(err)
-        }
-    }
-    else{
+    } else {
         alert("No name error.")
     }
-}
+  }
